e0d46fb6a847c4b9a098c4089282921cbc60196f
Markeihooooo <66111310@dpu.ac.th>
2023-11-05 17:19:27 +0700

    5:19

--- code_cal.py
+++ code_cal.py
@@ -1,18 +1,13 @@
-gray_code = "\033[1;30m" # กำหนดสี 
-reset_code = "\033[0m"
 ##################################
-"""import profile
-price = profile.price"""
+topping_list = ["วุ้นอโล", "สโนว์มุก", "ไข่มุกโนบิ", "เยลลี่ ผลไม้", "บุกราวน์ชูการ์"]
 import menu
 current_price = menu.price_use()
-
-print(current_price)
 
 topping_item = " "
 topping_showall = menu.topping_showall()
 
 
-topping_list = ["วุ้นอโล", "สโนว์มุก", "ไข่มุกโนบิ", "เยลลี่ ผลไม้", "บุกราวน์ชูการ์"]
+
 topping_choice = int(input("คุณต้องการอะไรบ้าง: "))
 
 if topping_choice == 1:
@@ -20,4 +15,11 @@ if topping_choice == 1:
     print("ตอนนี้คุณเลือก : ",topping_item)
     checktopping_up = input("คุณต้องการเลือกท๊อปปิ้งอีก หรือไม่ Y/n : ")
     if checktopping_up == "Y" or checktopping_up == "y":
-        pass
+       menu.topping_not1()
+       topping_choice1 = int(input("คุณต้องการใส่อะไรอีก 1 อย่าง : "))
+       if topping_choice1 == 1 :
+           print("คุณเลือกรายการซ้ำ กรุณาเลือกใหม่ : ")
+    elif checktopping_up == "N" or checktopping_up == "n":
+        pass   
+                 
+    
