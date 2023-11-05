a4d1c41c1ee8b27c69c085e796981c7601fb8b4a
Markeihooooo <66111310@dpu.ac.th>
2023-11-05 18:20:22 +0700

    6:20

--- menu.py
+++ menu.py
@@ -38,15 +38,15 @@ def topping_showall():
     print("5.บุกราวน์ชูการ์ ---- +5 ฿\n")
 
 def topping_not1():
-    print(f"\n{gray_code}1.วุ้นอโล ---- +5 ฿{reset_code}")
+    print(f"\n{gray_code}1.วุ้นอโล      ---- +5 ฿{reset_code}")
     print("2.สโนว์มุก     ---- +5 ฿")
     print("3.ไข่มุกโนบิ    ---- +5 ฿")
     print("4.เยลลี่ ผลไม้  ---- +5 ฿")
     print("5.บุกราวน์ชูการ์ ---- +5 ฿\n")
 
 def topping_not2():
-    print("\n1.วุ้นอโล     ---- +5 ฿")
-    print(f"\n{gray_code}2.สโนว์มุก ---- +5 ฿{reset_code}")
+    print("\n1.วุ้นอโล      ---- +5 ฿")
+    print(f"{gray_code}2.สโนว์มุก ---- +5 ฿{reset_code}")
     print("3.ไข่มุกโนบิ    ---- +5 ฿")
     print("4.เยลลี่ ผลไม้  ---- +5 ฿")
     print("5.บุกราวน์ชูการ์ ---- +5 ฿\n")
@@ -54,7 +54,7 @@ def topping_not2():
 def topping_not3():
     print("\n1.วุ้นอโล      ---- +5 ฿")
     print("2.สโนว์มุก     ---- +5 ฿")
-    print(f"\n{gray_code}3.ไข่มุกโนบิ    ---- +5 ฿{reset_code}")
+    print(f"{gray_code}3.ไข่มุกโนบิ    ---- +5 ฿{reset_code}")
     print("4.เยลลี่ ผลไม้  ---- +5 ฿")
     print("5.บุกราวน์ชูการ์ ---- +5 ฿\n")
 
@@ -62,7 +62,7 @@ def topping_not4():
     print("\n1.วุ้นอโล      ---- +5 ฿")
     print("2.สโนว์มุก     ---- +5 ฿")
     print("3.ไข่มุกโนบิ    ---- +5 ฿")
-    print(f"\n{gray_code}4.เยลลี่ ผลไม้  ---- +5 ฿{reset_code}")
+    print(f"{gray_code}4.เยลลี่ ผลไม้  ---- +5 ฿{reset_code}")
     print("5.บุกราวน์ชูการ์ ---- +5 ฿\n")
 
 def topping_not5():
@@ -70,74 +70,82 @@ def topping_not5():
     print("2.สโนว์มุก     ---- +5 ฿")
     print("3.ไข่มุกโนบิ    ---- +5 ฿")
     print("4.เยลลี่ ผลไม้  ---- +5 ฿")
-    print(f"\n{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
+    print(f"{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
 
 def topping_not12():
-    print(f"\n{gray_code}1.วุ้นอโล ---- +5 ฿{reset_code}")
-    print(f"\n{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
+    print(f"\n{gray_code}1.วุ้นอโล      ---- +5 ฿{reset_code}")
+    print(f"{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}")
     print("3.ไข่มุกโนบิ    ---- +5 ฿")
     print("4.เยลลี่ ผลไม้  ---- +5 ฿")
     print("5.บุกราวน์ชูการ์ ---- +5 ฿\n")
 
 def topping_not13():
-    print(f"\n{gray_code}1.วุ้นอโล ---- +5 ฿{reset_code}")
+    print(f"\n{gray_code}1.วุ้นอโล      ---- +5 ฿{reset_code}")
     print("2.สโนว์มุก     ---- +5 ฿")
-    print(f"\n{gray_code}3.ไข่มุกโนบิ    ---- +5 ฿{reset_code}")
+    print(f"{gray_code}3.ไข่มุกโนบิ    ---- +5 ฿{reset_code}")
     print("4.เยลลี่ ผลไม้  ---- +5 ฿")
     print("5.บุกราวน์ชูการ์ ---- +5 ฿\n")
 
 def topping_not14():
-    print(f"\n{gray_code}1.วุ้นอโล ---- +5 ฿{reset_code}")
+    print(f"\n{gray_code}1.วุ้นอโล      ---- +5 ฿{reset_code}")
     print("2.สโนว์มุก     ---- +5 ฿")
     print("3.ไข่มุกโนบิ    ---- +5 ฿")
-    print(f"\n{gray_code}4.เยลลี่ ผลไม้  ---- +5 ฿{reset_code}")
+    print(f"{gray_code}4.เยลลี่ ผลไม้  ---- +5 ฿{reset_code}")
     print("5.บุกราวน์ชูการ์ ---- +5 ฿\n")
 
 def topping_not15():
-    print(f"\n{gray_code}1.วุ้นอโล ---- +5 ฿{reset_code}")
+    print(f"\n{gray_code}1.วุ้นอโล      ---- +5 ฿{reset_code}")
     print("2.สโนว์มุก     ---- +5 ฿")
     print("3.ไข่มุกโนบิ    ---- +5 ฿")
     print("4.เยลลี่ ผลไม้  ---- +5 ฿")
-    print(f"\n{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
+    print(f"{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
 
 def topping_not23():
     print("\n1.วุ้นอโล      ---- +5 ฿")
-    print(f"\n{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
-    print(f"\n{gray_code}3.ไข่มุกโนบิ    ---- +5 ฿{reset_code}")
+    print(f"{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
+    print(f"{gray_code}3.ไข่มุกโนบิ    ---- +5 ฿{reset_code}")
     print("4.เยลลี่ ผลไม้  ---- +5 ฿")
     print("5.บุกราวน์ชูการ์ ---- +5 ฿\n")
 
 def topping_not24():
     print("\n1.วุ้นอโล      ---- +5 ฿")
-    print(f"\n{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
+    print(f"{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
     print("3.ไข่มุกโนบิ    ---- +5 ฿")
-    print(f"\n{gray_code}4.เยลลี่ ผลไม้  ---- +5 ฿{reset_code}")
+    print(f"{gray_code}4.เยลลี่ ผลไม้  ---- +5 ฿{reset_code}")
     print("5.บุกราวน์ชูการ์ ---- +5 ฿\n")
 
 def topping_not25():
     print("\n1.วุ้นอโล      ---- +5 ฿")
-    print(f"\n{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
+    print(f"{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
     print("3.ไข่มุกโนบิ    ---- +5 ฿")
     print("4.เยลลี่ ผลไม้  ---- +5 ฿")
-    print(f"\n{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
+    print(f"{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
 
 def topping_not34():
     print("\n1.วุ้นอโล      ---- +5 ฿")
     print("2.สโนว์มุก     ---- +5 ฿")
-    print(f"\n{gray_code}3.ไข่มุกโนบิ    ---- +5 ฿{reset_code}")
-    print(f"\n{gray_code}4.เยลลี่ ผลไม้  ---- +5 ฿{reset_code}")
+    print(f"{gray_code}3.ไข่มุกโนบิ    ---- +5 ฿{reset_code}")
+    print(f"{gray_code}4.เยลลี่ ผลไม้  ---- +5 ฿{reset_code}")
     print("5.บุกราวน์ชูการ์ ---- +5 ฿\n")
 
 def topping_not35():
     print("\n1.วุ้นอโล      ---- +5 ฿")
     print("2.สโนว์มุก     ---- +5 ฿")
-    print(f"\n{gray_code}3.ไข่มุกโนบิ    ---- +5 ฿{reset_code}")
+    print(f"{gray_code}3.ไข่มุกโนบิ    ---- +5 ฿{reset_code}")
     print("4.เยลลี่ ผลไม้  ---- +5 ฿")
-    print(f"\n{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
+    print(f"{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
 
 def topping_not45():
     print("\n1.วุ้นอโล      ---- +5 ฿")
     print("2.สโนว์มุก     ---- +5 ฿")
     print("3.ไข่มุกโนบิ    ---- +5 ฿")
-    print(f"\n{gray_code}4.เยลลี่ ผลไม้  ---- +5 ฿{reset_code}")
-    print(f"\n{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
+    print(f"{gray_code}4.เยลลี่ ผลไม้  ---- +5 ฿{reset_code}")
+    print(f"{gray_code}5.บุกราวน์ชูการ์ ---- +5 ฿{reset_code}\n")
+
+def size_cup():
+    print("\n======== Glass =========")
+    print("| 1.s      ----- Free ฿ |")
+    print("| 2.M      ----- 5 ฿    |")
+    print("| 3.L      ----- 10 ฿   |")
+    print("| 4.XL     ----- 15 ฿   |")
+    print("=========================\n")
