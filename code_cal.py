611480bf90e6aaf1d8a65cf1c4fdf99cd760d9f1
Markeihooooo <66111310@dpu.ac.th>
2023-11-06 10:09:27 +0700

    to day topping list

--- code_cal.py
+++ code_cal.py
@@ -1,90 +1,104 @@
 ##################################
-topping_list = ["วุ้นอโล", "สโนว์มุก", "ไข่มุกโนบิ", "เยลลี่ผลไม้", "บุกราวน์ชูการ์"]
+topping_list = ["วุ้นอโล", "สโนว์มุก","ไข่มุกโนบิ","เยลลี่ผลไม้", "บุกราวน์ชูการ์ ", "คาราเมล", "สตอเบอรี่", "ไข่มุกราวน์ชูการ์", "บุกคริสตัส", "ราสเบอรี่"]
 import menu
 current_price = menu.price_use()
 topping_item = " "
+topping_numberall = [] #ทำให้ท๊อปปิ้งสีดำ
+
+def topping_check(selected_items):
+     for i in range(1, 11):
+         if i in selected_items:
+             print("\033[1;30m", end='')  # สีเทา
+         else:
+             print("\033[0m", end='')  # สีปกติ
+        
+         if  i == 1:
+             print("1.วุ้นอโล        ---- +15 ฿", end='')
+         elif i == 2:
+             print("    2.สโนว์มุก       ---- +10 ฿")
+         elif i == 3:
+             print("3.ไข่มุกโนบิ      ---- +10 ฿", end='')
+         elif i == 4:
+             print("    4.เยลลี่ ผลไม้    ---- +10 ฿")
+         elif i == 5:
+           print("5.บุกราวน์ชูการ์   ---- +10 ฿", end='')
+         elif i == 6:
+            print("    6.คาราเมล      ---- +10 ฿")
+         elif i == 7:
+             print("7.สตอเบอรี่      ---- +10 ฿", end='')
+         elif i == 8:
+             print("    8.ไข่มุกราวน์ชูการ์ ---- +15 ฿")
+         elif i == 9:
+             print("9.บุกคริสตัส      ---- +15 ฿", end='')
+         elif i == 10:
+             print("    10.ราสเบอรี่     ---- +10 ฿\n")
+     print("\033[0;37;40m", end='')
+# topping_check(topping_numberall)
 
 
 
 while True:
-    topping_showall = menu.topping_showall()
-    topping_want = int(input("คุณต้องการใส่ท็อปปิ้งกี่อย่าง"))
+    topping_want = input("คุณต้องการใส่ท็อปปิ้งกี่อย่าง สูงสุดใส่ได้ 3 อย่าง\nต้องการใส่กี่อย่าง >>>: ")
+    
     try:
-        if topping_want == 1 :
-            topping_number1 = int(input("Number"))
-        elif topping_want == 2 :
-            topping_number1 = int(input("Number"))
-            topping_number2 = int(input("Number"))
-        elif topping_want == 3 :
-            topping_number3 = int(input("Number"))
-        elif topping_want == 4 :
-            topping_number4 = int(input("Number"))
-        elif topping_want == 5 :
-            topping_number5 = int(input("Number"))
-        elif topping_want == 6 :
-            topping_number6 = int(input("Number"))
-        elif topping_want == 7 :
-            topping_number7 = int(input("Number"))
-        elif topping_want == 8 :
-            topping_number8 = int(input("Number"))
-        elif topping_want == 9 :
-            topping_number9 = int(input("Number"))
-        elif topping_want == 10 :
-            topping_number10 = int(input("Number"))
-
-        # elif topping_want == 2 :
-        #     topping_number = int(input("Number"))
-        #     topping_number2 = int(input("Number"))
+        while True:
+            topping_showall = menu.topping_showall()
+            topping_want = int(topping_want)
+            if topping_want == 1 :  
+                topping_number1 = int(input("รายการท๊อปปิ้งที่คุณต้องการ : "))
+                topping_select = topping_list[topping_number1-1]
+                print("รายการที่คุณเลือก : ",topping_select)
+                topping_numberall.append(topping_number1)
+                topping_check(topping_numberall)
+                cheack_topping = input("คุณต้องการดำเนินการต่อหรือไม่ \n1.ดำเนินการต่อไป   2.เปลี่ยนท๊อปปิ้ง   3.ยกเลิกท๊อปปิ้ง \n>>>: ")
+                if cheack_topping == "1":
+                    break
+                elif cheack_topping == "2":
+                    topping_select = ""
+                    topping_numberall = []
+                    continue
+                elif cheack_topping == "3":
+                    topping_select = ""
+                    topping_numberall = ""
+                    break
+        
+                
+            elif topping_want == 2 :
+                topping_number1 = int(input("รายการท๊อปปิ้ง ที่ 1 : "))
+                print("รายการที่คุณเลือก : ",topping_list[topping_number1-1])
+                topping_numberall.append(topping_number1)
+                topping_number2 = int(input("รายการท๊อปปิ้ง ที่ 2 : "))
+                topping_numberall.append(topping_number1)
+                print("รายการที่คุณเลือก : ",topping_list[topping_number2-1])
+                break
+            elif topping_want == 3 :
+                topping_number1 = int(input("รายการท๊อปปิ้ง ที่ 1 : "))
+                print("รายการที่คุณเลือก : ",topping_list[topping_number1-1])
+                topping_number2 = int(input("รายการท๊อปปิ้ง ที่ 2 : "))
+                print("รายการที่คุณเลือก : ",topping_list[topping_number2-1])
+                topping_number3 = int(input("รายการท๊อปปิ้ง ที่ 3 : "))
+                print("รายการที่คุณเลือก : ",topping_list[topping_number3-1])
+                break
+            break
+        break
     except ValueError:
         print("***กรุณาใส่เลือกรายการเฉพาะตัวเลข***")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # topping_choice = int(input("คุณต้องการใส่อะไรบ้าง: "))
-    # try:
-    #     if topping_choice == 1:
-    #         topping_item = topping_item+ topping_list[topping_choice-1]
-    #         print("ตอนนี้คุณเลือก : \n==========",topping_item)
-    #         menu.topping_not1()
-          
-
-
-
-
-
-    #                     menu.topping_not1()
-    #                     topping_choice1 = int(input("คุณต้องการใส่อะไรอีก 1 อย่าง : "))
-    #                     if topping_choice1 == 1 :
-    #                         print("คุณเลือกรายการซ้ำ กรุณาเลือกใหม่ : ")
-    #                     elif topping_choice1 == 2 :
-    #                         topping_item = topping_item +","+ topping_list[topping_choice-2]
-    #                         print("ตอนนี้คุณเลือก : ",topping_item)
-    #                         menu.topping_not12()
-    #                         break
-    #                 except ValueError:
-    #                         print("***กรุณาใส่เลือกรายการเฉพาะตัวเลข***")
-    #                         pass
-    #         elif checktopping_up == "N" or checktopping_up == "n":
-    #             pass   
-    # except ValueError:
-    #     pass
-
-
-
-
-
-
-                 
     
+
+print(topping_select)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import menu
+topping_list = ["วุ้นอโล", "สโนว์มุก","ไข่มุกโนบิ","เยลลี่ผลไม้", "บุกราวน์ชูการ์ ", "คาราเมล", "สตอเบอรี่", "ไข่มุกราวน์ชูการ์", "บุกคริสตัส", "ราสเบอรี่"]
+
 while True:
     menu_data = menu.menu()
     menu_number = input("\nเลือกเมนูที่คุณต้องการ : ")
@@ -6,18 +8,48 @@ while True:
     try:
         menu_number = int(menu_number)
         if menu_number == 1:
-            print("\n\nเมนูที่คุณเลือก\n***ชานมใต้หวัน(Taiwan Mile Tea)     29฿ ***")
-            menu.set_price(menu.price_use() + 29)
+            print("\n\nเมนูที่คุณเลือก\n***ชานมใต้หวัน(Taiwan Mile Tea)     19฿ ***")
+            menu.set_price(menu.price_use() + 19)
         elif menu_number == 2:
-            pass
+            print("\n\nเมนูที่คุณเลือก\n***ชานมกาแฟ(Coffee Milk Tea)     19฿ ***")
+            menu.set_price(menu.price_use() + 19)
         elif menu_number == 3:
-            pass
+            print("\n\nเมนูที่คุณเลือก\n***ชานมโกโก้(Cocoa Milk Tea)     19฿ ***")
+            menu.set_price(menu.price_use() + 19)
         elif menu_number == 4:
-            pass
-        break  
+            print("\n\nเมนูที่คุณเลือก\n***ชานมเผือก(Taro Milk Tea)      19฿ ***")
+            menu.set_price(menu.price_use() + 19)
+        elif menu_number == 5:
+            print("\n\nเมนูที่คุณเลือก\n***ชานมลิ้นจี่(Lychee Milk Tea)  19฿ ***")
+            menu.set_price(menu.price_use() + 19)
+        elif menu_number == 6:
+            print("\n\nเมนูที่คุณเลือก\n***ชานมคาราเมล(caramel Milk Tea) 19฿ ***")
+            menu.set_price(menu.price_use() + 19)
+        elif menu_number == 7:
+            print("\n\nเมนูที่คุณเลือก\n***ชานมน้ำผึ้ง(Honey Milk Tea)   19฿ ***")
+            menu.set_price(menu.price_use() + 19)
+        elif menu_number == 8:
+            print("\n\nเมนูที่คุณเลือก\n***ชาดำเย็น(Thai Tea)         19฿ ***")
+            menu.set_price(menu.price_use() + 19)
+        elif menu_number == 9:
+            print("\n\nเมนูที่คุณเลือก\n***ชาไทย(Thai Milk Tea)        19฿ ***")
+            menu.set_price(menu.price_use() + 19)
+        elif menu_number == 10:
+            print("\n\nเมนูที่คุณเลือก\n***ชาเขียว(green Tea)          19฿ ***")
+            menu.set_price(menu.price_use() + 19)
+        elif menu_number == 11:
+            print("\n\nเมนูที่คุณเลือก\n***ชานมเเอปเปิ้ล(Appln Milk Tea) 19฿ ***")
+            menu.set_price(menu.price_use() + 19)
+        break
     except ValueError:
         print("***กรุณาใส่เลือกรายการเฉพาะตัวเลข***")
-
+print("\n***************** TOPPING *****************\nท๊อปปิ้งของเรามีดังนี้ \n")
+print(topping_list,"\n")
 topping_yn = input("คุณต้องการเพิ่ม ท๊อปปิ้ง ไหม \nY.ต้องการเพิ่ม\nN.ไม่ต้องการ\n\nY/N : ")
 if topping_yn == ("Y") or topping_yn == ("y"):
     import code_cal
+elif topping_yn == ("N") or topping_yn == ("n"):
+    pass
+
+
+menu.size_cup()
